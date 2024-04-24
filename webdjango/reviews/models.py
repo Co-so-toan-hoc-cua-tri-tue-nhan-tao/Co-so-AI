a5e12ca95304795a07c5547f7253c2125818f589
hoandkjxhbfhk <hoanoc321@hoanoc321gmail.com>
2024-04-24 21:50:25 +0700

    add recommend matrixfactorization

--- webdjango/reviews/models.py
+++ webdjango/reviews/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import numpy as np
 from django.contrib.auth.models import User
+from shop.models import Product
 class Wine(models.Model):
     name = models.CharField(max_length=200)
 
@@ -19,6 +20,8 @@ class Review(models.Model):
         (5,'5'),
     )
     wine = models.ForeignKey(Wine,on_delete = models.DO_NOTHING)
+    # product=models.Foreginkey(Product,on_delete=models.DO_NOTHING)
+
     pub_date = models.DateTimeField('date published')
     user_name = models.CharField(max_length=100)
     comment = models.CharField(max_length=200)
