58ae900cd5d123bd544d14a0691c5d65ee7a90c1
hoanganh05082004 <144500297+hoanganh05082004@users.noreply.github.com>
2024-04-24 11:48:31 +0000

    cai tien app, form và test cua review

--- webdjango/reviews/apps.py
+++ webdjango/reviews/apps.py
@@ -3,3 +3,26 @@ from django.apps import AppConfig
 
 class ReviewsConfig(AppConfig):
     name = 'reviews'
+
+
+
+"""
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+class ReviewsConfig(AppConfig):
+    name = 'reviews'
+
+    def ready(self):
+        # Đăng ký hàm xử lý sự kiện cho sự kiện post_migrate
+        post_migrate.connect(my_post_migrate_handler, sender=self)
+
+def my_post_migrate_handler(sender, **kwargs):
+    """
+    Hàm xử lý sự kiện post_migrate.
+    Thực hiện các hành động sau khi các migraions đã được áp dụng.
+    """
+    # Thêm bất kỳ hành động nào bạn muốn thực hiện ở đây
+    pass
+
+"""
