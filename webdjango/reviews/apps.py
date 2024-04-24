f3237fc1221a9d943f0896d468b1199c61b115c3
hoangbuituananh <130560924+hoangbuituananh132109@users.noreply.github.com>
2024-04-24 12:04:18 +0000

    tuananh

--- webdjango/reviews/apps.py
+++ webdjango/reviews/apps.py
@@ -4,25 +4,13 @@ from django.apps import AppConfig
 class ReviewsConfig(AppConfig):
     name = 'reviews'
 
-
-
 """
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 class ReviewsConfig(AppConfig):
     name = 'reviews'
 
     def ready(self):
-        # Đăng ký hàm xử lý sự kiện cho sự kiện post_migrate
-        post_migrate.connect(my_post_migrate_handler, sender=self)
-
-def my_post_migrate_handler(sender, **kwargs):
-    """
-    Hàm xử lý sự kiện post_migrate.
-    Thực hiện các hành động sau khi các migraions đã được áp dụng.
-    """
-    # Thêm bất kỳ hành động nào bạn muốn thực hiện ở đây
-    pass
-
+        # Gọi hàm cập nhật cụm khi ứng dụng sẵn sàng
+        update_clusters()
 """
